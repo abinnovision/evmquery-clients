05255508285e2e75e3bcbd6e1c12d6a6b1a4b5de
Andre Loreth <andre@loreth.co>
2026-04-25 17:49:31 +0200

    fix: add n8n eslint plugin

--- packages/n8n-nodes-evmquery/eslint.config.ts
+++ packages/n8n-nodes-evmquery/eslint.config.ts
@@ -4,9 +4,15 @@ import {
 	stylistic,
 	vitest,
 } from "@abinnovision/eslint-config-base";
+import { n8nCommunityNodesPlugin } from "@n8n/eslint-plugin-community-nodes";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
 	{ extends: [base, vitest, stylistic] },
+	{
+		extends: [n8nCommunityNodesPlugin.configs.recommended],
+		files: ["src/**/*.ts"],
+		ignores: ["src/**/*.spec.ts"],
+	},
 	{ files: ["*.{c,m,}{t,j}s"], extends: [configFiles] },
 ]);
